52eb50042519b14a3c49faef4e79422f43b7930b
Jebear76 <jerepondumie@gmail.com>
2024-11-04 17:22:19 +0200

    don't trust gitcopilot too much

--- mysql/kube-setup.sh
+++ mysql/kube-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/JeBear76/kubernetes-playground/refs/heads/master/mysql/secret.yaml
-kubectl apply -f https://raw.githubusercontent.com/JeBear76/kubernetes-playground/refs/heads/master/mysql/deployment.yaml
-kubectl apply -f https://raw.githubusercontent.com/JeBear76/kubernetes-playground/refs/heads/master/mysql/service.yaml
-kubectl apply -f https://raw.githubusercontent.com/JeBear76/kubernetes-playground/refs/heads/master/mysql/configmap.yaml
+kubectl apply -f https://raw.githubusercontent.com/JeBear76/kubernetes-playground/refs/heads/master/mysql/mysql-secret.k8s.yaml
+kubectl apply -f https://raw.githubusercontent.com/JeBear76/kubernetes-playground/refs/heads/master/mysql/mysql-storage.k8s.yaml
+kubectl apply -f https://raw.githubusercontent.com/JeBear76/kubernetes-playground/refs/heads/master/mysql/mysql-persistent-volume-claim.k8s.yaml
+kubectl apply -f https://raw.githubusercontent.com/JeBear76/kubernetes-playground/refs/heads/master/mysql/mysql-deployment.k8s.yaml
